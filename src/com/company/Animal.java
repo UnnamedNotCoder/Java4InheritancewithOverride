c28b6a2568c9fe393eaf279a55021976a78a066d
UnnamedNotCoder <chetvergov.vsewolod@yandex.ru>
2022-02-22 10:16:51 +0300

    Initial commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Animal {
+    public void eat(){
+        System.out.println("animal is eating");
+    }
+    public void sleep(){
+        System.out.println("animal is sleeping");
+    }
+}
